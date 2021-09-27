75a5ac5c86c4231dec36c3bbb37ed45232e9a196
ponchix <ponchorc28@gmail.com>
2021-09-27 16:53:09 -0500

    Redireccion login

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\UsuarioController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
