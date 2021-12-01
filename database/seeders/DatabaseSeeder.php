d5589151c2d65f3640d464b28805cef3855ba0eb
ponchix <ponchorc28@gmail.com>
2021-12-01 11:46:10 -0600

    Cambios Tablas/ Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\StatusAsignacion;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            marcasSeeder::class,
+            SeederTablaPermisos::class,
+            ServiciosSeeder::class,
+            StatusAsignacionSeeder::class,
+            StatusConductorSeeder::class,
+            StatusMantenimientoSeeder::class,
+            StatusSeeder::class,
+            SuperAdminSeeder::class,
+            TiposVehSeeder::class,
+        ]);
     }
 }
