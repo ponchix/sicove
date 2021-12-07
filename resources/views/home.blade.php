0b34b621e87d2b8fd1a90d03140afe0c9c2d7237
ponchix <ponchorc28@gmail.com>
2021-12-07 16:31:11 -0600

    ocultar calendario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,101 +31,105 @@
                             </div>
                         </div>
                         <div class="titulo">Resumen</div>
-                        <div class="row">
-                            <div class="col-md-4 col-xl-4">
-                                <div class="card bg-c-green order-card">
-                                    <div class="card-block">
-                                        <h5>Total de Vehiculos</h5>
-                                        @php
-                                            use App\Models\VehiculoModel;
-                                            $cant_vehiculos = VehiculoModel::count();
-                                        @endphp
-                                        <h2 class="text-right"> <i class=" fas fa-car-side f-left"></i>
-                                            <span>{{ $cant_vehiculos }}</span>
-                                        </h2>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                            <h5>Total de Vehiculos</h5>
+                                            @php
+                                                use App\Models\VehiculoModel;
+                                                $cant_vehiculos = VehiculoModel::count();
+                                            @endphp
+                                            <h2 class="text-right"> <i class=" fas fa-car-side f-left"></i>
+                                                <span>{{ $cant_vehiculos }}</span>
+                                            </h2>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                            <h5>Conductores</h5>
+                                            @php
+                                                use App\Models\Conductor;
+                                                $cant_conductores = Conductor::count();
+                                            @endphp
+                                            <h2 class="text-right"> <i
+                                                    class="fas fa-user f-left"></i><span>{{ $cant_conductores }}</span></h2>
+    
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                            <h5>Mecánicos</h5>
+                                            @php
+                                                use App\Models\Mecanico;
+                                                $cant_mecanicos = Mecanico::count();
+                                            @endphp
+                                            <h2 class="text-right"> <i
+                                                    class="fas fa-hard-hat f-left"></i><span>{{ $cant_mecanicos }}</span>
+                                            </h2>
+    
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4 col-xl-4">
-                                <div class="card bg-c-green order-card">
-                                    <div class="card-block">
-                                        <h5>Conductores</h5>
-                                        @php
-                                            use App\Models\Conductor;
-                                            $cant_conductores = Conductor::count();
-                                        @endphp
-                                        <h2 class="text-right"> <i
-                                                class="fas fa-user f-left"></i><span>{{ $cant_conductores }}</span></h2>
-
+                            <div class="row">
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                            <h5>Incidentes</h5>
+                                            @php
+                                                use App\Models\Incidente;
+                                                $cant_users = Incidente::count();
+                                            @endphp
+                                            <h2 class="text-right"> <i
+                                                    class="fas fa-car-crash f-left"></i></i><span>{{ $cant_users }}</span>
+                                            </h2>
+                                        </div>
                                     </div>
+    
                                 </div>
-                            </div>
-                            <div class="col-md-4 col-xl-4">
-                                <div class="card bg-c-green order-card">
-                                    <div class="card-block">
-                                        <h5>Mecánicos</h5>
-                                        @php
-                                            use App\Models\Mecanico;
-                                            $cant_mecanicos = Mecanico::count();
-                                        @endphp
-                                        <h2 class="text-right"> <i
-                                                class="fas fa-hard-hat f-left"></i><span>{{ $cant_mecanicos }}</span>
-                                        </h2>
-
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                            <h5>Gastos Totales</h5>
+                                            <h2 class="text-right"> <i class="fas fa-dollar-sign  f-left  "></i><span>{{ $total }} MXN</span>
+                                            </h2>
+                                        </div>
                                     </div>
+    
+                                </div>
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                            <h5>Proveedores</h5>
+                                            @php
+                                                use App\Models\Proveedor;
+                                                $proveedores = Proveedor::count();
+                                            @endphp
+                                            <h2 class="text-right"> <i class="fa fa-address-book f-left" aria-hidden="true"></i><span>{{ $proveedores }}</span>
+                                            </h2>
+                                        </div>
+                                    </div>
+    
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-4 col-xl-4">
-                                <div class="card bg-c-green order-card">
-                                    <div class="card-block">
-                                        <h5>Incidentes</h5>
-                                        @php
-                                            use App\Models\Incidente;
-                                            $cant_users = Incidente::count();
-                                        @endphp
-                                        <h2 class="text-right"> <i
-                                                class="fas fa-car-crash f-left"></i></i><span>{{ $cant_users }}</span>
-                                        </h2>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div class="col-md-4 col-xl-4">
-                                <div class="card bg-c-green order-card">
-                                    <div class="card-block">
-                                        <h5>Gastos Totales</h5>
-                                        <h2 class="text-right"> <i class="fas fa-dollar-sign  f-left  "></i><span>{{ $total }} MXN</span>
-                                        </h2>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div class="col-md-4 col-xl-4">
-                                <div class="card bg-c-green order-card">
-                                    <div class="card-block">
-                                        <h5>Proveedores</h5>
-                                        @php
-                                            use App\Models\Proveedor;
-                                            $proveedores = Proveedor::count();
-                                        @endphp
-                                        <h2 class="text-right"> <i class="fa fa-address-book f-left" aria-hidden="true"></i><span>{{ $proveedores }}</span>
-                                        </h2>
-                                    </div>
-                                </div>
 
-                            </div>
-                        </div>
-                        <div class="titulo">Agenda</div>
-                        <div class="container" >
-                            <p class="colapsar">
-                                <a class="btn btn-primary btn-colapsar d-flex justify-content-end" data-bs-toggle="collapse" href="#eventos" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                    <i class="fas fa-minus"></i>
-                                  </a>
-                            </p>
+                            <div class="titulo">
+                                Agenda  <a class="btn btn-primary btn-colapsar " data-bs-toggle="collapse" href="#eventos" role="button" aria-expanded="true" aria-controls="collapseExample">
+                                      <i class="fas fa-plus"></i> 
+                                         </a>
+                              </div>
 
                                 <div class="card collapsing" id="eventos">
+                                    
                                     <div class="card-body">
                                         @include('Calendario.index')
                                     </div>
@@ -133,10 +137,7 @@
                                
 
 
-                               
-
-                               
-                        </div>
+                
 
 
 
@@ -172,6 +173,12 @@
             mostrarHora();
         });
     </script>
-
+<script>
+    $('.collapsing').on('shown.bs.collapse', function(){
+$(this).parent().find(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+}).on('hidden.bs.collapse', function(){
+$(this).parent().find(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+});
+</script>
 
 @endsection
